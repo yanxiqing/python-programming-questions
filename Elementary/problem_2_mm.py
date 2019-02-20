859b8fbf40e2870b4afde5ac6022dd1eea3fd5f2
Lenis Merino <lenis.merino@hotmail.com>
2019-02-20 23:05:50 +0800

    - Added problem_2_mm.py in /Elementary

---
+++ Elementary/problem_2_mm.py
@@ -0,0 +1,8 @@
+"""
+Problem 2: Write a program that asks the user for her name and greets her with her name.
+"""
+
+if __name__ == "__main__":
+
+    user_name = input("Please type your name: ")
+    print("Hello " + user_name + "!")
