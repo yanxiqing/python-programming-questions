0e70dc83b09a10e868697496061476ee53c03170
yanxiqing <657835209@qq.com>
2019-02-21 14:46:34 +0800

    1.create a new file named problem_2_yxq.py in Elementary. 2.using input function to do this problem

---
+++ Elementary/problem_2_yxq.py
@@ -0,0 +1,6 @@
+"problem 2 : write a program that asks the user for her name and greets her with her name"
+
+
+if __name__ == "__main__" :
+    name = input("what's your name ?\n")
+    print("Hello,"+ name + "!")
